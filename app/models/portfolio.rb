e5caee16a48334a6ef975fcc3967e78bda685975
watchdogoblivion <samueldorilas>
2018-07-30 14:57:07 -0400

    Implemented image defaults for portfolios

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -7,4 +7,15 @@ class Portfolio < ApplicationRecord
 	end
 
 	scope :ruby_on_Rails, -> {where(subtitle: 'Ruby on Rails')}
+
+	after_initialize :set_defaults
+
+	def set_defaults
+		self.main_image ||= "http://via.placeholder.com/600x400"
+		self.thumb_image ||= "http://via.placeholder.com/350x200"
+	end
+	#you set defaults in migration or model like above
+	#||= is like if a.nil? a ="" else do nothing
+
+
 end
