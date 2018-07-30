7ba08021529a0df689ac25bd9349c84a824deb99
watchdogoblivion <samueldorilas>
2018-07-30 15:23:40 -0400

    Implemented concern for managing data

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -1,4 +1,5 @@
 class Portfolio < ApplicationRecord
+	include Placeholder
 	validates_presence_of :title, :body, :main_image, :thumb_image
 
 	#both of the following do the same thing for different subtitles
@@ -11,8 +12,8 @@ class Portfolio < ApplicationRecord
 	after_initialize :set_defaults
 
 	def set_defaults
-		self.main_image ||= "http://via.placeholder.com/600x400"
-		self.thumb_image ||= "http://via.placeholder.com/350x200"
+		self.main_image ||= Placeholder.image_generator(height: '600', width: '400')
+		self.thumb_image ||= Placeholder.image_generator(height: '350', width: '200')
 	end
 	#you set defaults in migration or model like above
 	#||= is like if a.nil? a ="" else do nothing
