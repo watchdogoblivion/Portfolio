d0efc31ec06eaa08aec03c57f0f082bb0f2bebd6
watchdogoblivion <samueldorilas>
2018-08-08 02:42:25 -0400

    Incomplete commit bug

--- app/jobs/comment_broadcast_job.rb
+++ app/jobs/comment_broadcast_job.rb
@@ -8,6 +8,6 @@ class CommentBroadcastJob < ApplicationJob
 	private
 
 	def render_comment(comment)
-		CommentsController.render partial: 'comments/comment', locale: {comment: comment}
+		CommentsController.render partial: 'comments/comment', locals: {comment: comment}
 	end
 end
