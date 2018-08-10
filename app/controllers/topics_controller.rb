06859ea8de0377c17e3b93cce0d177d3a347f2bb
watchdogoblivion <samueldorilas>
2018-08-10 12:42:34 -0400

    Created topics route, controller, and index to manage creation, edit, and deletion of topics

---
+++ app/controllers/topics_controller.rb
@@ -0,0 +1,56 @@
+class TopicsController < ApplicationController
+  before_action :set_topic, only: [:update, :destroy]
+  layout "application"
+  access all: [], user: {except: [:index, :destroy, :create, :update]}, site_admin: :all
+
+  def index
+      @topics = Topic.all   
+      @topic = Topic.new  
+  end
+
+  def create
+    @topic = Topic.new(topic_params)
+
+    respond_to do |format|
+      if @topic.save
+        format.html { redirect_to topics_url, notice: 'Topic was successfully created.' }
+        format.json { render :show, status: :created, location: @topic }
+      else
+        format.html { render :new }
+        format.json { render json: @topic.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def update
+    respond_to do |format|
+      if @topic.update(topic_params)
+        format.html { redirect_to topics_url, notice: 'Topic was successfully updated.' }
+        format.json { render :show, status: :ok, location: @topic }
+      else
+        format.html { render :edit }
+        format.json { render json: @topic.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def destroy
+    @topic.destroy
+    respond_to do |format|
+      format.html { redirect_to topics_url, notice: 'Topic was successfully destroyed.' }
+      format.json { head :no_content }
+    end
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_topic
+      @topic = Topic.find(params[:id])
+    end
+
+    # Never trust parameters from the scary internet, only allow the white list through.
+    def topic_params
+      params.require(:topic).permit(:title)
+    end
+
+end
