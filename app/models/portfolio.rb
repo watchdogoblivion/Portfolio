b8b173a669cb50ae466e75dabedaa85f28f576ab
watchdogoblivion <samueldorilas>
2018-08-12 15:22:01 -0400

    Implemeted fixes for images and grid for portfolio items

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -6,8 +6,8 @@ class Portfolio < ApplicationRecord
 
 	validates_presence_of :title, :body
 
-	mount_uploader :thumb_image, PortfolioUploader
-	mount_uploader :main_image, PortfolioUploader
+	 mount_uploader :thumb_image, PortfolioUploader
+	 mount_uploader :main_image, PortfolioUploader
 
 	#both of the following do the same thing for different subtitles
 	def self.angular
