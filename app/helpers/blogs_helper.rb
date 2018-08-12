260e40d773096fa16e77898766b59dc1be00963a
watchdogoblivion <samueldorilas>
2018-08-12 01:04:17 -0400

    Implemented icons in breadcrumbs, and added two helper methods, blog_head_links and line_limiter

--- app/helpers/blogs_helper.rb
+++ app/helpers/blogs_helper.rb
@@ -22,4 +22,24 @@ module BlogsHelper
 		markdown_to_html = Redcarpet::Markdown.new(coderayified, options)
 		markdown_to_html.render(text).html_safe
 	end
+    
+    def blog_head_links
+    	if logged_in?(:site_admin)
+	    	(link_to 'Write a new Blog', new_blog_path) + " | ".html_safe +
+	    	(link_to 'Published Blogs', published_path) + " | ".html_safe +
+	    	(link_to 'Drafted Blogs', draft_path) + " | ".html_safe + 
+	    	(link_to 'All Blogs', blogs_path)
+	    end 
+    end
+
+    def line_limiter(object, lines)
+    	count = 0
+    	string = ""
+    	object.each_line do |s|
+    		string << s
+    		count = count + 1
+    		return string if count == lines
+    	end
+    	string
+    end
 end
